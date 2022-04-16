1f14134936406b421169ecb4b56eaf70884a1dc4
Emma Grannis <grannis.emma@gmail.com>
2022-04-16 19:14:50 +0000

    Following lint checks

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescriptions = [];
-
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
+
 function newTask(title, description) {
   const task = {
     title: title,
@@ -12,25 +8,25 @@ function newTask(title, description) {
     complete: false,
 
     logState: function() {
-    console.log(`${this.title} has${this.complete ? " " : " not "}been completed`)
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     },
 
-    markCompleted: function () {
+    markCompleted: function() {
       this.complete = true;
     }
   };
- return task;
+  return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
-}
+// // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+// function completeTask(task) {
+//   task.complete = true;
+// }
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
+// // Print the state of a task to the console in a nice readable way
+// function logTaskState(task) {
+//   console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+// }
 
 // DRIVER CODE BELOW
 
